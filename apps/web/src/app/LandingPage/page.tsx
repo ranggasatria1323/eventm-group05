37910579c696643d169175c6050d6ac571688b6d
ranggasatria1323 <ranggasatria1323@gmail.com>
2025-01-17 13:21:12 +0700

    initial commit

--- apps/web/src/app/LandingPage/page.tsx
+++ apps/web/src/app/LandingPage/page.tsx
@@ -1,11 +1,9 @@
 import Hero from '@/components/Hero';
-import TopEvent from '@/components/TopEvent';
 
 export default function LandingPage() {
   return (
     <>
       <Hero />
-      <TopEvent />
     </>
   );
 }
